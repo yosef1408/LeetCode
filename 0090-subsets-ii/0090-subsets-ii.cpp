a8d476ca2bf1bc72ce0f4b98f7243dd722277520
Yosef Break <56613561+yosef1408@users.noreply.github.com>
2024-12-15 12:10:04 +0200

    Time: 4 ms (22.26%), Space: 11.9 MB (25.00%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    
+    
+    void backtrack(vector<int>&nums,vector<vector<int>>& res,int start,vector<int> currSet ){
+        
+        if(start == nums.size())
+            return;
+        
+        unordered_map<int, bool> seen;
+        for (int i = 0; i < nums.size(); ++i) {
+            seen[nums[i]] = false;
+        }
+
+        for(int i = start; i < nums.size(); i++){
+            
+            if(seen[nums[i]] == true){
+                continue;
+            }
+            
+            currSet.push_back(nums[i]);
+            seen[nums[i]] = true;
+            res.push_back(currSet);
+            backtrack(nums,res,(i+1),currSet);
+            
+            currSet.pop_back();
+        }
+    }
+    
+    
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        vector<vector<int>> res;
+        res.push_back({});
+        sort(nums.begin(), nums.end());
+
+        backtrack(nums,res,0,{});
+        
+        return res;
+        
+    }
+};
